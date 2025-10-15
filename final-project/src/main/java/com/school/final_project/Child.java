89efc033feb9370b0551aea9b1a85cf711b491e6
Heatherann Jackson <heatherann.m.jackson@gmail.com>
2025-10-15 14:24:25 -0500

    updating classes related to user accounts.

--- final-project/src/main/java/com/school/final_project/Child.java
+++ final-project/src/main/java/com/school/final_project/Child.java
@@ -1,5 +1,5 @@
 package main.java.com.school.final_project;
 
 public class Child extends User {
-    
+    Parent parent;
 }
--- final-project/src/main/java/com/school/final_project/User.java
+++ final-project/src/main/java/com/school/final_project/User.java
@@ -1,7 +1,13 @@
+package main.java.com.school.final_project;
+
 public class User {
     String userName;
     String firstName;
     String lastName;
     String password;
-    Int age;    
+    Int age;  
+    
+    updateAccount() {
+
+    };
 }
