73be917f4ce96b3c9078ec7a7babd6b03913ec01
Heatherann Jackson <heatherann.m.jackson@gmail.com>
2025-11-11 15:52:06 -0600

    updated methods in parent account.

--- final-project/src/main/java/com/school/final_project/Parent.java
+++ final-project/src/main/java/com/school/final_project/Parent.java
@@ -1,13 +1,15 @@
 package com.school.final_project;
 
 import java.util.ArrayList;
-import java.util.List;
 
 public class Parent extends User {
 
     private String parentId;
     private String name;
     private ArrayList<Child> children;
+    private ArrayList<Chore> chores;
+    private ArrayList<StoreItem> storeInventory;
+    private ArrayList<Transaction> transactions;
 
     public Child createChildAccount(String childName, String userName){
         if (childName == null || childName.trim().isEmpty()) {
@@ -18,5 +20,13 @@ public class Parent extends User {
         children.add(newChild);
 
         return newChild;
+    }
+
+    public void addChore(Chore chore) {
+        chores.add(chore);
+    }
+
+    public void addStoreItem(StoreItem item) {
+        storeInventory.add(item);
     }
 }
