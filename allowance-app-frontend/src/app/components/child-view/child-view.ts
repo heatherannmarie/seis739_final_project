3f74b5b8093387e4b4d7366ef5bd097019157aa5
Heatherann Jackson <heatherann.m.jackson@gmail.com>
2025-12-05 20:43:14 -0600

    Created components and services

---
+++ allowance-app-frontend/src/app/components/child-view/child-view.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-child-view',
+  imports: [],
+  templateUrl: './child-view.html',
+  styleUrl: './child-view.css',
+})
+export class ChildView {
+
+}
