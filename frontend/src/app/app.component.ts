6dd70c1b28a1e3fc138fa83e97fa2d25b1348aaa
Heatherann Jackson <heatherann.m.jackson@gmail.com>
2025-12-09 21:22:34 -0600

    Revert "Began adding authentication"
    This reverts commit 2839cda91b6a8686561c9807d65a8cc9585173d4.

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -2,8 +2,6 @@ import { Component } from '@angular/core';
 import { Router, RouterOutlet, RouterLink } from '@angular/router';
 import { inject } from '@angular/core';
 import { AuthService } from './services/auth.service';
-import { OAuthService } from 'angular-oauth2-oidc';
-import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -13,25 +11,12 @@ import { HttpClient } from '@angular/common/http';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  helloText = '';
-
   title = 'frontend';
   private authService = inject(AuthService);
   private router = inject(Router);
-  private oauthService = inject(OAuthService);
-  private httpClient = inject(HttpClient);
 
   logout() {
-    this.oauthService.logOut()
-  }
-
-  getHelloText() {
-    this.httpClient.get<{ message: string }>('http://localhost:8080/hello', {
-      headers: {
-        'Authorization': `Bearer ${this.oauthService.getAccessToken()}`
-      }
-    }).subscribe(result => {
-      this.helloText = result.message;
-    });
+    this.authService.logout();
+    this.router.navigate(['/']);
   }
 }
